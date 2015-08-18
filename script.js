750da0f19f8a03060b47ca8922e8a7b69bdbe663
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-18 17:26:17 -0400

    made solutuion

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function piglatinize(word){
-
+  var vowels = ["a", "e", "i", "o", "u"];
+  if(vowels.indexOf(word[0]) > -1 || (/^.*y/).test(word)){
+    word = word + "hay";
+  }else
+  if(/^s?qu/.test(word)){
+    word = word.replace(/^s?qu/, "") + word.match(/^s?qu/)[0] + "ay";
+  }else{
+    word = word.substring(1) + word[0] + "ay";
+  }
   return word;
 }
 
