356868d4b7cd1a55ec1877af227206ddde99db10
mattscilipoti <matt.scilipoti@ga.co>
2015-08-18 22:35:19 -0400

    oink: add speech support

--- script.js
+++ script.js
@@ -11,11 +11,24 @@ function piglatinize(word){
   return word;
 }
 
+function speak(word, voiceName){
+  // see: https://developers.google.com/web/updates/2014/01/Web-apps-that-talk---Introduction-to-the-Speech-Synthesis-API
+  voiceName = typeof voiceName !== 'undefined' ? voiceName : "Ralph";
+  if ('speechSynthesis' in window) {
+ // Synthesis support. Make your web apps talk!
+   var msg = new SpeechSynthesisUtterance(word);
+   msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceName; })[0];
+   window.speechSynthesis.speak(msg);
+  }
+}
+
 $(document).ready(function(){
   var nose = $("#nose");
   var mouth = $("#mouth");
   nose.on("click", function(){
     var word = mouth.val();
-    mouth.val(piglatinize(word));
+    translated = piglatinize(word);
+    mouth.val(translated);
+    speak(translated, "Moira");
   });
 });
