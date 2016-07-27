87e1cd53369b4c68ccc37ef90445b39c70ef8fbb
Jesse Shawl <jesse@jshawl.com>
2016-07-27 13:24:13 -0400

    red

--- spec/piglatinize-spec.js
+++ spec/piglatinize-spec.js
@@ -1,4 +1,8 @@
 var piglatinize = require("../lib/piglatinize")
 describe("piglatinize", function(){
-
+  describe("a word that starts with a vowel", function(){
+    it("adds way to the end", function(){
+      expect(piglatinize("egg")).toEqual("eggway")
+    })
+  })
 })
